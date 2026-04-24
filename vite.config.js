87c2685059782dbebd7f79517507715fd5982430
noelgaspar7 <noelgaspar7@gmail.com>
2026-04-24 10:13:48 -0400

    testing route to rigth url

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
-  base: 'https://noelgaspar.github.io/',
+  base: '/cosmotejido/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
