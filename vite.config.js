d693f1d0df47ef2a9eddce06f15d541629536990
noelgaspar7 <noelgaspar7@gmail.com>
2026-04-24 09:57:26 -0400

    fixing config.js file

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
-  base: 'https://NoelGaspar.github.io/cosmotejido/'
+  base: 'https://NoelGaspar.github.io/cosmotejido/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
