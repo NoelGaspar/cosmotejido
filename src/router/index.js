d2da07d455a4a0ea5370c410a97bc03019931f44
noelgaspar7 <noelgaspar7@gmail.com>
2026-04-24 10:05:11 -0400

    rutas para deploy

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import ViewerView from '../views/ViewerView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('#'),
   routes: [
     {
       path: '/',
